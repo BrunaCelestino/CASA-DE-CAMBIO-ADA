5fce9c4c7bd0f871b13e880113118172aa078137
BrunaMassuchini <brunaccelestino@gmail.com>
2023-02-01 23:52:15 -0300

    feat: create classes

--- compra/src/main/java/com/ada/compra/OrdemCompra.java
+++ compra/src/main/java/com/ada/compra/OrdemCompra.java
@@ -1,4 +1,24 @@
 package com.ada.compra;
 
 public class OrdemCompra {
+
+//    Após efetuar o cadastro, o cliente pode registrar a ordem de compra, que consiste em informar seu cpf,
+//    o tipo de moeda (USD ou EUR),
+//    o valor em moeda estrangeira e o número da agência (4 dígitos) que ocorrerá a retirada.
 }
+
+//Ao final de uma requisição bem sucedida, deverá ser retornado um request body similiar ao abaixo:
+//
+//        Response 201
+//
+//        {
+//        "id_compra": 1,
+//        "id_cliente": 1,
+//        "cpf_cliente": "43488428095",
+//        "dataSolicitacao": "2021-08-27T16:11:23.866",
+//        "tipo_moeda": "EUR",
+//        "valor_moeda_estrangeira": 100.0,
+//        "valor_cotacao": 6.5857,
+//        "valor_total_operacao": 658.57,
+//        "numero_agencia_retirada": "7057"
+//        }
---
+++ compra/src/main/java/com/ada/compra/OrdemCompraDTO.java
@@ -0,0 +1,4 @@
+package com.ada.compra;
+
+public class OrdemCompraDTO {
+}
