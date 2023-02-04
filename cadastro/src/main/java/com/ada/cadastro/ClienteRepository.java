f904e5b90aa85532432522f7d8a8eaa70bbb5a46
Mayara Gandolfi <112402452+MayaraGandolfi@users.noreply.github.com>
2023-02-04 13:31:39 -0300

    create Service de Cliente e ajustando Repository para buscar por Cpf

--- cadastro/src/main/java/com/ada/cadastro/ClienteRepository.java
+++ cadastro/src/main/java/com/ada/cadastro/ClienteRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface ClienteRepository extends CrudRepository<Cliente,Long> {
 
-    Cliente findByCpf (Cliente cliente);
+    Cliente findByCpf (String cpf);
 
 }
