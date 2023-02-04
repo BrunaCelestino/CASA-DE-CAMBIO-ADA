c58143a2311a03326480769621c3838db0b15d31
Ana Carolina Okubaro <anacarolinaokubaro@gmail.com>
2023-02-04 18:15:10 -0300

    ordemCompraDTO

--- compra/src/main/java/com/ada/compra/OrdemCompraDTO.java
+++ compra/src/main/java/com/ada/compra/OrdemCompraDTO.java
@@ -1,4 +1,49 @@
 package com.ada.compra;
 
+import lombok.Data;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDate;
+@Data
 public class OrdemCompraDTO {
+
+    private Long idCompra;
+    private Long idCliente;
+    private String cpfCliente;
+    private LocalDate dataSolicitacao;
+    private TipoMoeda tipoMoedaEstrangeira;
+    private Double valorMoedaEstrangeira;
+    private Double valorCotação;
+    private Double valorTotalOperação;
+    private String numeroAgenciaDeRetirada;
+
+    public static OrdemCompraDTO of(OrdemCompra ordemCompra) {
+        OrdemCompraDTO dto = new OrdemCompraDTO();
+        dto.setIdCompra(ordemCompra.getIdCompra());
+        dto.setIdCliente(ordemCompra.getIdCliente());
+        dto.setCpfCliente(ordemCompra.getCpfCliente());
+        dto.setDataSolicitacao(ordemCompra.getDataSolicitacao());
+        dto.setTipoMoedaEstrangeira(ordemCompra.getTipoMoedaEstrangeira());
+        dto.setValorCotação(ordemCompra.getValorCotação());
+        dto.setValorTotalOperação(ordemCompra.getValorTotalOperação());
+        dto.setNumeroAgenciaDeRetirada(ordemCompra.getNumeroAgenciaDeRetirada());
+    }
+
+    public OrdemCompraDTO toEntity (){
+        OrdemCompra entity = new OrdemCompra();
+        entity.setIdCompra(idCompra);
+        entity.getIdCliente(idCliente);
+        entity.getCpfCliente(cpfCliente);
+        entity.setDataSolicitacao(dataSolicitacao);
+        entity.setTipoMoedaEstrangeira(tipoMoedaEstrangeira);
+        entity.setValorCotação(valorCotação);
+        entity.setValorTotalOperação(valorTotalOperação);
+        entity.setNumeroAgenciaDeRetirada(numeroAgenciaDeRetirada);
+        return entity;
+    }
+
 }
+
+
