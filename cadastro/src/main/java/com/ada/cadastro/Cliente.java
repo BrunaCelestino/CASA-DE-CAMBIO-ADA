b9ee28d533e5ccfb2961b9d76e59addbb24d96bb
Mayara Gandolfi <112402452+MayaraGandolfi@users.noreply.github.com>
2023-02-03 21:05:28 -0300

    feat: entidade Cliente

--- cadastro/src/main/java/com/ada/cadastro/Cliente.java
+++ cadastro/src/main/java/com/ada/cadastro/Cliente.java
@@ -1,10 +1,67 @@
 package com.ada.cadastro;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.time.LocalDate;
+
+@Entity
 public class Cliente {
 
-//    Para que o cliente realize a compra, primeiro ele deve se cadastrar
-//    no sistema informando o nome, cpf, data de nascimento,
-//    estado civil e sexo. Para tanto, um microsserviço de cadastro
-//    deve ser criado, gerando um id de cliente ao final de um processamento bem sucedido.
-//    Também deverá ser criado um endpoint de consulta do cliente pelo seu cpf.
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String nome;
+    private String cpf;
+    private LocalDate dataNascimento;
+    private String estadoCivil;
+    private String sexo;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getEstadoCivil() {
+        return estadoCivil;
+    }
+
+    public void setEstadoCivil(String estadoCivil) {
+        this.estadoCivil = estadoCivil;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
 }
