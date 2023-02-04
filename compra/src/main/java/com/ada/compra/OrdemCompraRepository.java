faf740bb474ce8aa3baccc748449e44a0f12b368
Ana Carolina Okubaro <anacarolinaokubaro@gmail.com>
2023-02-04 18:22:25 -0300

    ordemCompraRepository

--- compra/src/main/java/com/ada/compra/OrdemCompraRepository.java
+++ compra/src/main/java/com/ada/compra/OrdemCompraRepository.java
@@ -1,9 +1,8 @@
 package com.ada.compra;
-
-import org.aspectj.weaver.ast.Or;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class OrdemCompraRepository extends CrudRepository<OrdemCompra, Long> {
+public interface OrdemCompraRepository extends CrudRepository<OrdemCompra, Long> {
+
 }
