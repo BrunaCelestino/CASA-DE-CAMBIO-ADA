285a539a114eafe87fd08b297cfe6e568fdacf03
Mayara Gandolfi <112402452+MayaraGandolfi@users.noreply.github.com>
2023-02-04 14:08:56 -0300

    create Controller de Cliente e ajustando erro no Service para salvar o cliente

--- cadastro/src/main/java/com/ada/cadastro/ClienteController.java
+++ cadastro/src/main/java/com/ada/cadastro/ClienteController.java
@@ -1,4 +1,24 @@
 package com.ada.cadastro;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/cadastro")
 public class ClienteController {
+    private final ClienteService clienteService;
+
+    public ClienteController(ClienteService clienteService) {
+        this.clienteService = clienteService;
+    }
+
+    @PostMapping
+    public Cliente salvar(@RequestBody Cliente cliente) {
+       return clienteService.salvar(cliente);
+    }
+
+    @GetMapping("/{cpf}")
+    public Cliente buscarPorCpf(@PathVariable String cpf){
+        return clienteService.buscaPorCpf(cpf);
+    }
+
 }
