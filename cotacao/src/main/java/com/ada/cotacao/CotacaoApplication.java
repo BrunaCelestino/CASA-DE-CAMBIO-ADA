5a6f774fd5c96516cab7c174f94e4060d6717961
BrunaMassuchini <brunaccelestino@gmail.com>
2023-02-04 23:18:46 -0300

    Deletando imports não usados

--- cotacao/src/main/java/com/ada/cotacao/CotacaoApplication.java
+++ cotacao/src/main/java/com/ada/cotacao/CotacaoApplication.java
@@ -2,13 +2,9 @@ package com.ada.cotacao;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 
-@RestController
+
 @SpringBootApplication
 public class CotacaoApplication {
 
