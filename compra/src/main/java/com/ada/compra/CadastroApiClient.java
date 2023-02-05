6b4c0063babb9275fa8357cb67dcd2539b9b6b22
Ana Carolina Okubaro <anacarolinaokubaro@gmail.com>
2023-02-05 09:18:11 -0300

    ajuste ordemCOmpraservice

--- compra/src/main/java/com/ada/compra/CadastroApiClient.java
+++ compra/src/main/java/com/ada/compra/CadastroApiClient.java
@@ -1,9 +1,21 @@
 package com.ada.compra;
 
+import com.ada.cadastro.ClienteDTO;
+import org.aspectj.weaver.ast.Instanceof;
+import org.springframework.remoting.RemoteAccessException;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.Optional;
 
 public class CadastroApiClient {
 
-    private String URI = "http://localhost:8081/cadastro/";
+    private ClienteDTO clienteDTO;
+    private String URI = "http://localhost:8081/cadastro/{cpf}";
     private WebClient client = WebClient.create();
+
+
+
+
+
 }
--- compra/src/main/java/com/ada/compra/OrdemCompraService.java
+++ compra/src/main/java/com/ada/compra/OrdemCompraService.java
@@ -1,26 +1,44 @@
 package com.ada.compra;
+import com.ada.exceptions.CadastroInvalidoException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Service;
-import java.time.LocalDate;
+import org.springframework.web.client.RestClientException;
 
+import java.time.LocalDate;
+import java.util.Optional;
+
+@RequiredArgsConstructor
 @Service
 public class OrdemCompraService {
 
+    private final CrudRepository<OrdemCompra, Long> repository;
+    private final CadastroApiClient cadastroApiClient;
+
+
+    public Optional<OrdemCompra> getById (Long id){
+        return repository.findById(id);
+    }
     public OrdemCompra obterCotacao(OrdemCompra ordemCompra) {
         CotacaoApiClient cotacaoApiClient = new CotacaoApiClient();
 
-        // Obtendo a Cotação da api de Cotação
         CotacaoDTO cotacao = cotacaoApiClient.getCotacao(ordemCompra.getTipoMoedaEstrangeira().toString());
-
-        // Colocando o valor obtido no atributo
         ordemCompra.setValorCotação(cotacao.getCotacaoAlta());
-
-        // Calculando o valor total da operação
         ordemCompra.setValorTotalOperação(cotacao.getCotacaoAlta() * ordemCompra.getValorMoedaEstrangeira());
-
-        // Colocando a data do dia
         ordemCompra.setDataSolicitacao(LocalDate.now());
 
         return ordemCompra;
     }
+
+    public OrdemCompra salvar (OrdemCompra entity) throws CadastroInvalidoException {
+        try {
+            if (cadastroApiClient. ((entity.getCpfCliente()))) {
+                throw new CadastroInvalidoException("Cadastro não localizado");
+                }
+            }catch (RestClientException e){
+                throw new CadastroInvalidoException("Api de Cadastro não disponível");
+            }
+    }
+
 
 }
